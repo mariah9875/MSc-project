85d8b50d997f86246d237abc599e4313a128ca51
mariah9875 <62725616+mariah9875@users.noreply.github.com>
2021-06-04 08:37:23 +0200

    Update README.sh

--- README.sh
+++ README.sh
@@ -1,17 +1,58 @@
-# This readme file contains step-by-step procedures that were done in this MSc thesis analysis
+""" 
+Differential expression analysis between humans and chimpanzees and the role of TEs.
 
+Description:
+This README file contains step-by-step procedures that were done in this MSc thesis analysis. The procedure is done in following steps:
 
+- Preprocessing raw reads from fastq files
+- Run a Snakemake pipeline
+- Run Data.prep.R script
+- Run DEA.R script
+- Run TEintersect.R script
+
+Installation:
+The Snakemake pipeline (Snakefile) and the R scripts are ready for download.
+
+Programs used in the analysis:
+- Python
+- snakemake 
+- R (v.1.3.1093)
+- Cutadapt (v.2.9)
+- COMPSRA (v.1.0.2)
+
+Programs run in snakemake:
+- FastQc (v.0.11.3)
+- MultiQC (v.1.2)
+- STAR (v.2.6.0c)
+- SAMtools (v.1.6)
+- deepTools (v.2.5.4)
+- Subread (v.1.6.3)
+- stringTie (v.1.3.3b)
+
+Programs run in R:
+- DESeq2 
+- data.table (v.1.13.2) 
+- multcomp (v.1.4)
+- gplots (v.3.1.1)
+- RcolorBrewer (v.1.1)
+- pheatmap (v.1.0.12)
+- ggplot2 (v.3.3.2)
+- dplyr (v.1.0.2)
+- plotly (v.4.9.2.1)
+
+"""
+# Usage
 # Raw fastq files were run with FASTQC program to check if adapters needed to be removed along with the quality of the reads.
 fastqc {input} -t 6 -o {output}
 
-# Raw fastq files were trimmed with cutadapt program
+# Raw fastq files were trimmed with Cutadapt program
 # 4 bases of each end:
  ls *.fastq | sed s/.fastq// | while read file; do cutadapt -u 4 -u -4 -o $file.trimmed.fastq $file.fastq; echo $file processed; done
-# trim adapter
+# Trim adapter
  ls *.trimmed.fastq | sed s/.trimmed.fastq// | while read file; do cutadapt -a what to trim -o $file.trimmed_1.fastq --minimum-length length $file.trimmed.fastq; echo $file processed; done
 
 """
-Trimmed reads ran with Snakefile:
+The trimmed reads were run with the Snakefile:
 The snakemake pipeline takes the trimmed files and performs the follwing steps:
   QC: FastQC and MultiQC
   Align reads to a reference genome (hg38) with STAR aligner Mapping 1 (M1) & Mapping 2 (M2)
@@ -41,22 +82,25 @@ A file named config.yaml is created by the user, and the file has to include a l
 samples: ["sample_1.fastq", "sample_2.fastq", "sample_3.fastq"]
 
 The output files from the Snakemake pipeline are countmatrixes of .csv files, .bw and .bam files.
-The .csv file can be used for DEA, the .bw files are uploaded to the UCSC genome browser and the bam files are used for the COMPSRA program to get countmatrix.
+The .csv file can be used for DEA, the .bw files are uploaded to the UCSC genome browser and the bam files are used for the COMPSRA program to get a count matrix.
 
 """
-# COMPSRA program run with bam files, for each sample file 6 .txt annotation files will be produced for each small RNA type counted for.
-# repeat for each sample being counted 
-java -jar /sw/pkg/compsra/1.0.2/COMPSRA.jar -ref hg38 -ann -ac 1,2,3,4,5,6 -in results/sorted_bam/version_2/*files.sorted.bam -out results/COMPSRA/version_2/*files_sorted
+# COMPSRA program was run with bam files, for each sample file 6 .txt quantification files will be produced for each small RNA type counted for.
+# For each sample:
+java -jar /sw/pkg/compsra/1.0.2/COMPSRA.jar -ref hg38 -ann -ac 1,2,3,4,5,6 -in results/sorted_bam/version_2/{files}.sorted.bam -out results/COMPSRA/version_2/{files}_sorted
 
 # Merge COMPSRA annotation files from each sample into 1 .txt file for that small RNA type. A sample.list file is needed, which contains the pwd of the files being merged
-# together. Repeat 5 times for each type """
+# together. Repeat 5 times for each small RNA type """
 java -jar /sw/pkg/compsra/1.0.2/COMPSRA.jar -ref hg38 -fun -fm -fms 1-11 -fdclass 1 -pro COMPSRA_MERGE -inf results/COMPSRA/version_2/sample.list -out results/COMPSRA/version_2
 
-""" The resulting 6 .txt countmatrix files are used as an input for the R script Data.prep.R. The resulting countmatrix generated from Data.prep.R is
-used as an input for DEA.R. """
+""" 
+The resulting 6 .txt count matrix files are used as an input for the R script Data.prep.R. The resulting count matrix generated from Data.prep.R is
+used as an input for DEA.R.
+"""
 
-# The resulting up_regulated.txt and all_regulated_location.txt files generated from DEA.R were used for bedTools intersect, to overlap with TEs and SNPs locations
+# The resulting up_regulated.txt file generated from DEA.R were used for bedTools intersect, to overlap with TEs locations
 bedtools intersect -a TEs.bed -b up_regulated_location.txt -wa -wb > TEs_overlapp.txt
-bedtools intersect -a gwas.bed -b all_regulated_location.txt -wa -wb
 
-""" The resulting .txt file from bedTools intersect was used as an input for R script TEintersect.R"""
+""" 
+The resulting .txt file from bedTools intersect was used as an input for R script TEintersect.R
+"""
